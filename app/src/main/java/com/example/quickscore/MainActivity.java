d3b27e5dd95dd6c66575e7a4d99ef5dca7f893a7
robert <robert.kroko@gmail.com>
2019-08-02 10:43:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/quickscore/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.quickscore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button b_Single = (Button)findViewById(R.id.b_Single);
+        Button b_Match = (Button)findViewById(R.id.b_Match);
+
+        b_Single.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(),SingleActivity.class);
+                startActivity(in);
+            }
+        });
+
+
+        b_Match.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent in = new Intent(getApplicationContext(),MatchActivity.class);
+                startActivity(in);
+            }
+        });
+
+    }
+}
